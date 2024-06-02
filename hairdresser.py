c33e3d9bb5676432c2e02c693e8623502b407502
jukle <julien.kle02@gmail.com>
2024-06-02 17:26:32 +0200

    Fix

--- hairdresser.py
+++ hairdresser.py
@@ -6,9 +6,7 @@ import queue
 
 
 NUM_HAIRDRESSERS = 4
-
 NUM_WAITING_SEATS = 15
-
 AIRLOCK_CAPACITY = 2
 
 waiting_seats_semaphore = threading.Semaphore(NUM_WAITING_SEATS)
@@ -61,7 +59,6 @@ class Client(threading.Thread):
             waiting_queue.append(self.client_id)
 
 
-
 # Create and start hairdresser threads
 hairdressers = [Hairdresser(i) for i in range(NUM_HAIRDRESSERS)]
 for hairdresser in hairdressers:
@@ -82,96 +79,3 @@ for hairdresser in hairdressers:
     hairdresser.join()
 
 print("All clients have been processed.")
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""" class Hairdresser(threading.Thread):
-    def __init__(self, hairdresser_id):
-        threading.Thread.__init__(self)
-        self.hairdresser_id = hairdresser_id
-
-    def run(self):
-        while True:
-            # Try to acquire a client (block if none are available)
-            client = waiting_queue.get()
-            if client is None:  # None is used to signal shutdown
-                break
-            self.style_hair(client)
-
-    def style_hair(self, client_id):
-        with hairdresser_lock:
-            print(f"Hairdresser {self.hairdresser_id} is styling Client {client_id}.")
-            time.sleep(random.uniform(0.5, 1.5))  # Simulate hair styling time
-            print(f"Hairdresser {self.hairdresser_id} has finished styling Client {client_id}.")
-            waiting_seats_semaphore.release()  # Free up a waiting seat
-
-class Client(threading.Thread):
-    def __init__(self, client_id):
-        threading.Thread.__init__(self)
-        self.client_id = client_id
-
-    def run(self):
-        print(f"Client {self.client_id} enters the salon.")
-        if not waiting_seats_semaphore.acquire(blocking=False):
-            print(f"Client {self.client_id} leaves as there are no free seats.")
-            return
-        
-        print(f"Client {self.client_id} is waiting in the airlock.")
-        with airlock_semaphore:
-            print(f"Client {self.client_id} is in the airlock.")
-            time.sleep(random.uniform(0.1, 0.5))  # Simulate time in airlock
-        
-        print(f"Client {self.client_id} is now waiting for a hairdresser.")
-        waiting_queue.put(self.client_id)  # Client enters the waiting queue
-
-def main():
-    global waiting_queue
-    waiting_queue = threading.Queue()
-
-    hairdressers = [Hairdresser(i) for i in range(NUM_HAIRDRESSERS)]
-    for hairdresser in hairdressers:
-        hairdresser.start()
-
-    client_id = 0
-    try:
-        while True:
-            time.sleep(random.uniform(0.1, 0.3))  # New clients arrive at random intervals
-            client = Client(client_id)
-            client.start()
-            client_id += 1
-    except KeyboardInterrupt:
-        print("Salon is closing. No more new clients.")
-        for _ in range(NUM_HAIRDRESSERS):
-            waiting_queue.put(None)  # Signal hairdressers to stop
-
-        for hairdresser in hairdressers:
-            hairdresser.join()
-
-if __name__ == "__main__":
-    main() 
-"""
